efabca7c1cfb3df20d5217cdffd115b63df4e061
AHETESHAMS <aheteshams007@gmail.com>
2019-04-27 13:10:19 +0530

    Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef,OnDestroy} from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
-  constructor() { }
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
   
-  ngOnInit() {
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
+
